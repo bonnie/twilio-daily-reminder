95f28ec403af53fe196b386f720fcaa8aefbc990
Bonnie Schulkin <biz@bonnieschulkin.com>
2020-10-23 14:36:17 -0700

    Add date to output; don't commit log files

--- meditation_reminder.js
+++ meditation_reminder.js
@@ -4,11 +4,13 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
+const date = new Date();
+
 client.messages
   .create({
      body: 'Reminder to take some time for yourself 😊',
      from: process.env.FROM_NUMBER,
      to: process.env.TO_NUMBER
    })
-  .then(message => console.log(message.sid))
+  .then(message => console.log(`${date}: ${message.sid}`))
   .catch(error => console.error(error));
